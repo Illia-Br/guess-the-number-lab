1f83d216b5cf9c547572596c4091d420e8587732
Illia Brytun <elbrytun@gmail.com>
2022-01-30 22:03:44 -0500

    add render method

--- app.js
+++ app.js
@@ -13,19 +13,26 @@ const game = {
     do {
       playerGuess = getGuess();
       this.prevGuesses.push(playerGuess);
+      render(playerGuess);
     } while (playerGuess !== secretNum);
-
+    
   },
   getGuess: function() {
-    let input;
-      while (!(input >= this.smallestNum) || !(input <= this.biggestNum)) {
-      input = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}:`));
-      console.log(input);
+    let input = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}:`));
+    while (!(input >= this.smallestNum) || !(input <= this.biggestNum)) {
+    input = parseInt(prompt(`Wrong format or your guess is out of range. Enter a guess between ${this.smallestNum} and ${this.biggestNum}:`));
+    console.log(input);
     }
     return input;
   },
-  render: function() {
-    
+  render: function(playerChoice) {
+    if (playerChoice === this.secretNum) {
+    alert(`Congrats! You guessed the number in ${this.prevGuesses.length} guesses!`);
+    } else if (playerChoice > this.secretNum) {
+      alert(`Your guess is too high. Previous guesses are: ${this.prevGuesses.join(", ")}`);
+    } else {
+      alert(`Your guess is too low. Previous guesses are: ${this.prevGuesses.join(", ")}`);
+    }
   }
 }
 
