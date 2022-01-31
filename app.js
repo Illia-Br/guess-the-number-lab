54def2b7a23cb8bf46b271c40f78af8146f806fa
Illia Brytun <elbrytun@gmail.com>
2022-01-30 20:47:36 -0500

    add getGuess method

--- app.js
+++ app.js
@@ -8,8 +8,16 @@ const game = {
   prevGuesses: [],
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
-      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
+  },
+  getGuess: function() {
+    let input;
+      while (!(input >= this.smallestNum) || !(input <= this.biggestNum)) {
+      input = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}:`));
+      console.log(input);
+    }
+    return input;
   }
 }
 
-console.log(game);
+game.getGuess();
